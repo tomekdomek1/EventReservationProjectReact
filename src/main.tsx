8c3168177285177acf864ba992d2f66006ee8497
Łukasz Mieczkowski <89240@student.pb.edu.pl>
2026-01-24 17:37:27 +0100

    Refactor routing in main.tsx to include UserProfileOverview, EditProfilePage, and ChangePasswordPage; add AdminDashboard route for better admin navigation

--- src/main.tsx
+++ src/main.tsx
@@ -5,14 +5,18 @@ import RouteLayout from './components/layout/RootLayout/RootLayout'
 import EventsPage from './pages/Events/EventsPage'
 import { createBrowserRouter, RouterProvider } from 'react-router-dom'
 import EventTable from './pages/Admin/EventTable/EventTable'
+import AdminDashboard from './pages/Admin/Dashboard/AdminDashboard'
 import LoginScreen from './pages/Auth/LoginPage'
 import RegistrationPage from './pages/Auth/RegistrationPage'
 import UserProfilePage from './pages/User/Profile/UserProfilePage'
-import NotFoundPage from './pages/NotFoundPage'
+import UserProfileOverview from './pages/User/Profile/UserProfileOverview';
+import EditProfilePage from './pages/User/Profile/EditProfilePage';
+import ChangePasswordPage from './pages/User/Profile/ChangePasswordPage';
 import { ThemeModeProvider } from './theme/ThemeModeProvider'
 import EventsSessionsTable from './pages/Admin/SessionTable/EventsSessionTable'
 import { SnackbarProvider } from 'notistack';
 import ProtectedRoute from './components/auth/ProtectedRoute';
+import NotFoundPage from './pages/notFoundPage'
 
 const router = createBrowserRouter([
   {
@@ -21,19 +25,24 @@ const router = createBrowserRouter([
       { path: '/', element: <EventsPage /> },
       { path: '/login', element: <LoginScreen /> },
       { path: '/register', element: <RegistrationPage /> },
-
-      // Routes for authenticated users (any role)
       {
         element: <ProtectedRoute />,
         children: [
-          { path: '/profile', element: <UserProfilePage /> },
+          { 
+            path: '/profile', 
+            element: <UserProfilePage />,
+            children: [
+               { index: true, element: <UserProfileOverview /> },
+               { path: 'edit', element: <EditProfilePage /> },
+               { path: 'password', element: <ChangePasswordPage /> }
+            ] 
+          },
         ]
       },
-
-      // Routes for Admin only
       {
         element: <ProtectedRoute allowedRoles={['Admin']} />,
         children: [
+          { path: '/admin/dashboard', element: <AdminDashboard /> },
           { path: '/admin/events', element: <EventTable /> },
           { path: 'admin/events/:id', element: <EventsSessionsTable /> },
         ]
